b713229168735ce7f2f32429e528a13a10b876bb
Sebastien Guimont <info@sebastienguimont.com>
2016-03-26 15:45:24 -0400

    use valid port for dokku

--- config/ci.js
+++ config/ci.js
@@ -2,7 +2,20 @@ module.exports = {
   env: 'ci',
 
   connections: {
+    api: {
+      labels: ['api'],
+      port: process.env.DOKKU_NGINX_PORT,
+      host: null,
+      routes: {
+        cors: {
+          origin: ['*']
+        }
+      }
+    },
+
     redis: {
+      host: 'pub-redis-13256.us-east-1-3.4.ec2.garantiadata.com',
+      port: 13256,
       partition: 'cliostats-ci'
     }
   },
